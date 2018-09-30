6d611d504c540f03c774370835c2d8b5a7b855f8
Raul <reperez79@gmail.com>
2018-09-30 15:10:05 -0700

    Added scene transitions

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour {
+
+    public string SceneName= "1 Opening office";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Moving to " + SceneName, gameObject);
+        SceneManager.LoadScene(SceneName);
+    }
+}
