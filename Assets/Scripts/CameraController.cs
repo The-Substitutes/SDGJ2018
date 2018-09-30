f88f28b41f157ada4aa3b18ac2f539eb592ae8d9
Raul <reperez79@gmail.com>
2018-09-30 04:38:43 -0700

    Hallway and other stuff

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,20 +8,25 @@ public class CameraController : MonoBehaviour {
 
 	[Space]
 	public bool lockCameraFromEditor = true;
+    public Vector3 followInDirection;
+
 	public Vector3 cameraOffset;
 
 	// Use this for initialization
 	void Start () {
 		if(lockCameraFromEditor) {
 			cameraOffset =  this.transform.position - Follow.transform.position;
-			cameraOffset.z = this.transform.position.z;
+            if(followInDirection.z != 0){
+                cameraOffset.z = this.transform.position.z;
+            }
+			
 		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		this.transform.position = new Vector3(	Follow.transform.position.x + cameraOffset.x,
-												Follow.transform.position.y + cameraOffset.y,
-												 cameraOffset.z);
+		this.transform.position = new Vector3(followInDirection.x * Follow.transform.position.x + cameraOffset.x,
+                                              followInDirection.y * Follow.transform.position.y + cameraOffset.y,
+                                              followInDirection.z * Follow.transform.position.z + cameraOffset.z);
 	}
 }
---
+++ Assets/Scripts/NPCAnimSped.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnimSped : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+        anim.speed = Random.Range(.9f, 1.1f);
+	}
+	
+}
